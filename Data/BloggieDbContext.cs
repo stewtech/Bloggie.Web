203d99ec54d7f5e082094d22cf0603c16d2c5d31
Cecil Stewart <72238656+stewtech@users.noreply.github.com>
2025-03-27 12:19:43 -0500

    Added DbContext Class

---
+++ Data/BloggieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.PortableExecutable;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
