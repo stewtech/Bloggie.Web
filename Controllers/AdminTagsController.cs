8414b9891b3cf9ec1dcf7e6b004891a26627d04a
Cecil Stewart <72238656+stewtech@users.noreply.github.com>
2025-03-29 12:35:30 -0500

    Added update functionality

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -50,5 +50,66 @@ namespace Bloggie.Web.Controllers
 
             return View(tags);
         }
+
+        // Create get method
+        [HttpGet]
+        // Parameter has to match the name of the route created
+        public IActionResult Edit(Guid id) 
+        { 
+            // Use bloggieDbContext to connect to database to read details to display on screen and allow user to update or edit details
+            //1st method var tag = bloggieDbContext.Tags.Find(id);
+            //2nd method will find the tag using the id and the first one it finds it will give it back
+            var tag = bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            // display tag into edit page
+            if (tag != null) 
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+
+            }
+
+            return View(null);
+        }
+        [HttpPost] // Specifies that this method handles HTTP POST requests.
+        // Accepts an EditTagRequest object from the form submission.
+        public IActionResult Edit(EditTagRequest editTagRequest) 
+        {
+            // Create a new Tag object using values from the editTagRequest
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+            // Find the existing tag in the database using the provided ID.
+            var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+            // It queries the bloggieDbContext.Tags table to find a tag with the given Id.
+            // Check if the existing tag was found
+            if (existingTag != null)
+            {
+                // Update the existing tag's Name and DisplayName with the new values.
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                // Save the updated tag details to the database.
+                bloggieDbContext.SaveChanges();
+
+                // Redirect back to the Edit page for the given tag ID.
+                // Show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+               
+            }
+            // If the tag does not exist, still redirect back to the Edit page.
+            // Show error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });  
+        }
+
     }
 }
